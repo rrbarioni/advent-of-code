4223bc99aff10cf238681645972763dc447c815f
Ricardo R. Barioni <rrbarioni@gmail.com>
2020-12-02 10:46:56 -0300

    fix day 1 part 2 for same i,j,k cases

--- src/day1.py
+++ src/day1.py
@@ -14,10 +14,11 @@ def solve_part1(entries):
         else:
             d[e] += 1
 
-    for e in entries:
-        if (n - e) in d:
-            if (e != (n - e)) or d[e] > 1:
-                prod = e * (n - e)
+    for e_i in entries:
+        e_j = n - e_i
+        if e_j in d:
+            if (e_i != e_j) or d[e_i] > 1:
+                prod = e_i * e_j
 
                 return prod
 
@@ -27,6 +28,8 @@ def solve_part2(entries):
         whose sum is equal to 2020?
     '''
     entries = [int(e) for e in entries]
+
+    n = 2020
 
     d = {}
     for e in entries:
@@ -39,10 +42,20 @@ def solve_part2(entries):
         e_i = entries[i]
         for j in range(i+1, len(entries)):
             e_j = entries[j]
-            if (2020 - e_i - e_j) in d:
-                prod = e_i * e_j * (2020 - e_i - e_j)
+            e_k = n - e_i - e_j
+            if e_k in d:
+                all_diff = (e_i != e_j) and (e_i != e_k) and (e_j != e_k)
+                ij_eq = (e_i == e_j) and (d[e_i] > 1)
+                ik_eq = (e_i == e_k) and (d[e_i] > 1)
+                jk_eq = (e_j == e_k) and (d[e_j] > 1)
 
-                return prod
+                # for "n" not divisible by 3, this case will never happen
+                all_eq = (e_i == e_j) and (e_i == e_k) and (d[e_i] > 2)
+
+                if all_diff or ij_eq or ik_eq or jk_eq or all_eq:
+                    prod = e_i * e_j * e_k
+
+                    return prod
 
 if __name__ == '__main__':
     entries = open('inputs/day1.txt').readlines()
