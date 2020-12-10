42b7c0013d92d548aa286b5d77cac99cb7dc7e64
Ricardo R. Barioni <rrbarioni@gmail.com>
2020-12-10 14:10:33 -0300

    cleaner day 10 part 2 code

--- src/day10.py
+++ src/day10.py
@@ -15,15 +15,13 @@ def solve_part1(entries):
 def solve_part2(entries):
     entries = [int(e.rstrip()) for e in entries]
 
-    s = set(entries)
-    s.add(0)
+    s = set(entries).union([0])
 
     neigh = [len([n for n in [1,2,3] if a + n in s]) for a in s][:-1]
     neigh = ''.join([str(l) for l in neigh]).split('1')
-
     neigh = [[int(v) for v in l] for l in neigh if len(l) > 0]
-    mults = [sum(n) - 1 if len(n) > 1 else n[0] for n in neigh]
 
+    mults = [sum(n) - 1 if len(n) > 1 else n[0] for n in neigh]
     mult = 1
     for m in mults:
         mult *= m
