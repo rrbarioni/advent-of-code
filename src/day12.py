6a0754693f5298192c02aa4391983be78c4a9690
Ricardo R. Barioni <rrbarioni@gmail.com>
2020-12-20 08:58:05 -0300

    day 12 part 2 code

--- src/day12.py
+++ src/day12.py
@@ -3,8 +3,6 @@ def solve_part1(entries):
 
     commands = [(e[0], int(e[1:])) for e in entries]
 
-    print(commands[0])
-
     x, y = [0, 0]
 
     orientations = [[0, 1], [1, 0], [0, -1], [-1, 0]]
@@ -34,10 +32,44 @@ def solve_part1(entries):
     return result
 
 def solve_part2(entries):
-    return
+    entries = [e.rstrip() for e in entries]
+
+    commands = [(e[0], int(e[1:])) for e in entries]
+
+    w_x, w_y = [10, 1]
+    s_x, s_y = [0, 0]
+
+    for action, value in commands:
+        if action == 'N':
+            w_y += value
+        elif action == 'S':
+            w_y -= value
+        elif action == 'E':
+            w_x += value
+        elif action == 'W':
+            w_x -= value
+        elif action == 'L':
+            turns = round(value / 90)
+            for _ in range(turns):
+                w_aux = w_x
+                w_x = -w_y
+                w_y = w_aux
+        elif action == 'R':
+            turns = round(value / 90)
+            for _ in range(turns):
+                w_aux = w_x
+                w_x = w_y
+                w_y = -w_aux
+        elif action == 'F':
+            s_x += (w_x * value)
+            s_y += (w_y * value)
+
+    result = abs(s_x) + abs(s_y)
+
+    return result
 
 if __name__ == '__main__':
     entries = open('inputs/day12.txt').readlines()
 
     print('part 1: %s' % solve_part1(entries))
-    # print('part 2: %s' % solve_part2(entries))
+    print('part 2: %s' % solve_part2(entries))
